3bc6adb259aca0d9880459ffe2ffe87d15682b48
Divyansh15322 <idivyansh88@gmail.com>
2026-03-07 19:21:29 +0530

    fix cors origin

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: [ "http://localhost:5173", "https://ai-resume-builder-omega-six.vercel.app/" ], credentials: true }));
+app.use(cors({ origin: [ "http://localhost:5173", "https://ai-resume-builder-omega-six.vercel.apps" ], credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
